9b60f3279c83111a67f335d7025048e0c9879eed
Nigar Jafar <nigar.c@code.edu.az>
2018-11-02 23:15:19 +0400

    Migrations

--- database/migrations/2018_11_01_195937_create_ratings_table.php
+++ database/migrations/2018_11_01_195937_create_ratings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRatingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+
+         Schema::create('ratings', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('image');
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('star');
+            $table->integer('from_user_id');
+            $table->integer('to_user_id');
             $table->timestamps();
+            
+
         });
     }
 
@@ -32,6 +33,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('ratings');
     }
 }
--- database/migrations/2018_11_02_052609_create_notifications_table.php
+++ database/migrations/2018_11_02_052609_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNotificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+
+        Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('image');
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('offer_id');
+            $table->char('type',1);
+            $table->char('status',1);
             $table->timestamps();
+           
         });
     }
 
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('notifications');
     }
-}
