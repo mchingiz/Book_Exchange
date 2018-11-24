461f479f5162332d41ee42f163aa8e5794c571f5
senemgenberova <senemgenberova@gmail.com>
2018-11-24 10:19:45 +0400

    book insert

--- app/Book.php
+++ app/Book.php
@@ -7,17 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable = [];
+    // protected $fillable = ['user_id','category_id','city_id','author','name','description','condition','year','exchange_preferences','exchange_status','view'];
+
+    protected $guarded = [];
 
     public function user(){
         return $this->belongsTo(User::class);
     }
 
     public function category(){
-        return $this->hasOne(Category::class);
+        return $this->belongsTo(Category::class);
     }
 
     public function city(){
-        return $this->hasOne(City::class);
+        return $this->belongsTo(City::class);
     }
 }
