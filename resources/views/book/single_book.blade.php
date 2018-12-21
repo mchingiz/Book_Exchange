c35b55334e01c3cf4dc99fafe1224a57ce54962e
Mammadli Chingiz <memmedlicngz@gmail.com>
2018-12-22 00:44:46 +0400

    single book change

--- resources/views/book/single_book.blade.php
+++ resources/views/book/single_book.blade.php
@@ -34,7 +34,11 @@
                </div>
                <div class="job-post-item-body d-block d-md-flex">
                  <!-- <div class="mr-3"><span class="fl-bigmug-line-portfolio23"></span> <a href="#">New York Times</a></div> -->
-                 <div class="mr-3"><span class="fl-bigmug-line-user144"></span> <span>{{$book->user->name}} {{$book->user->surname}}</span><br> </div>
+                 <div class="mr-3"><span class="fl-bigmug-line-user144"></span> 
+                    <a href="{{ route('user_profile', $book->user) }}">
+                      <span>{{$book->user->name}} {{$book->user->surname}}</span>
+                    </a><br> 
+                  </div>
                  <div class="mr-3"></div>
                  <div><span class="fl-bigmug-line-big104"></span> <span>{{$book->city->name}} </span></div>                 
                </div>
@@ -59,70 +63,70 @@
     <div class="comments">
       @if(Auth::check())
 
-          @if(Session::get('status') == 'success')
-              <div class="alert alert-success">
-                {{ Session::get('message') }}
+        @if(Session::get('status') == 'success')
+          <div class="alert alert-success">
+            {{ Session::get('message') }}
+          </div>
+        @endif
+
+        <form action="{{ route('offer.store') }}" method="POST">
+          {!! csrf_field() !!}
+          <div class="comment-wrap">
+              <div class="photo">
+                  <div class="avatar" style="background-image: url('https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/128.jpg')"></div>
               </div>
-            @endif
-            
-      <form action="{{ route('offer.store') }} " method="POST">
-        {!! csrf_field() !!}
-        <div class="comment-wrap">
-            <div class="photo">
-                <div class="avatar" style="background-image: url('https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/128.jpg')"></div>
-            </div>
-            <div class="comment-block">
-                
-                    <input type="hidden" name="offered_to_book" value="{{$book->id}}">
-                    <select class="custom-select" name="offered_book">
-                       @foreach(Auth::user()->books as $user_book)
-                        <option value="{{$user_book->id}}">{{$user_book->name}}</option>
-                       @endforeach
-                    </select>
-               
-            </div>          
-        </div>    
-        <div class="comment-wrap add-comment">
-        <div class="addComment">
-            <p class="mt-5"><button href="#" class="btn btn-primary  py-2 px-4">Təklif əlavə et</button></p>
+              <div class="comment-block">
+                  
+                <input type="hidden" name="offered_to_book" value="{{$book->id}}">
+                <select class="custom-select" name="offered_book">
+                   @foreach(Auth::user()->books as $user_book)
+
+                    <option value="{{$user_book->id}}">{{$user_book->name}}</option>
+                   @endforeach
+                </select>
+                 
+              </div>          
+          </div>    
+          <div class="comment-wrap add-comment">
+          <div class="addComment">
+              <p class="mt-5"><button href="#" class="btn btn-primary  py-2 px-4">Təklif əlavə et</button></p>
+          </div>
         </div>
-      </div>
        </form>
       @endif
     </div>
     <div class="comments">
         
-@if( is_array($book->offers))
-        @foreach($book->offers as $offer)
-       
 
-          <div class="comment-wrap">
-              <div class="photo">
-                  <div class="avatar" style="background-image: url('')"></div>
-              </div>
-              <div class="comment-block" >
-                  <p class="comment-text">{{$offer->offered_book->author}} - {{ $offer->offered_book->name}}
+    <h1 style="margin-left: 50px;">Təkliflər</h1>
+    @foreach($book->offers as $offer)
+      <div class="comment-wrap">
+          <div class="photo">
+              <div class="avatar" style="background-image: url('')"></div>
+          </div>
+          <div class="comment-block" >
+              <p class="comment-text">{{$offer->offered_book->author}} - {{ $offer->offered_book->name}}
 
-@if($offer->status==1)
+              @if($offer->status==1)
 
-<svg style="float:right" viewBox="0 0 26 26" enable-background="new 0 0 26 26" width="50px" height="50px">
-  <path d="m.3,14c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1v-8.88178e-16c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.4 0.4,1 0,1.4l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.5-0.1-0.7-0.3l-7.8-8.4-.2-.3z" fill="#26baee"/>
-</svg>
-<div style="clear: both"></div>
-@endif
+              <svg style="float:right" viewBox="0 0 26 26" enable-background="new 0 0 26 26" width="50px" height="50px">
+                <path d="m.3,14c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1v-8.88178e-16c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.4 0.4,1 0,1.4l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.5-0.1-0.7-0.3l-7.8-8.4-.2-.3z" fill="#26baee"/>
+              </svg>
+              <div style="clear: both"></div>
+              @endif
 
-                  </p>
-                  <div class="bottom-comment">
-                      <div class="comment-date">{{$offer->created_at}} </div>
-                      <ul class="comment-actions">
-                          <li class="complain">Şikayət et</li>
-                          <li class="reply">Cavabla</li>
-                      </ul>
-                  </div>
+              </p>
+              <div class="bottom-comment">
+                  <div class="comment-date">{{$offer->created_at}} </div>
+                  <ul class="comment-actions">
+                      <li class="complain">Şikayət et</li>
+                      <li class="reply">Cavabla</li>
+                  </ul>
               </div>
           </div>
-        @endforeach
-        @endif
+      </div>
+    @endforeach
+
     </div>
 @endsection
 
